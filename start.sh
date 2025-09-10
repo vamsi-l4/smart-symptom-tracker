e650ed7a34c12e63a7a3a5c2df69ffe955063763
vamsi-l4 <krishnavamsi3400@gmail.com>
2025-09-10 22:20:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ echo "Current directory: $(pwd)"
 echo "Environment variables:"
 env
 
-# Run FastAPI from project root with PYTHONPATH set
-PYTHONPATH=. uvicorn src.app:app --host 0.0.0.0 --port $PORT
+# Run FastAPI from current directory with PYTHONPATH set
+PYTHONPATH=. uvicorn app:app --host 0.0.0.0 --port $PORT
